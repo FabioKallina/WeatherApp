57b3fc08f3ecc89c6d37cd2e1270e9ebaa4ddb1f
FabioKallina <fabiokdp8@gmail.com>
2025-04-15 18:28:43 -0700

    added a toggle button to switch graph from celsius to fahrenheit

--- src/components/HourlyForecast.jsx
+++ src/components/HourlyForecast.jsx
@@ -1,17 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./HourlyForecast.css"
 
 import { XAxis, YAxis, Tooltip, ResponsiveContainer, Area, AreaChart, LabelList, Legend, Label } from "recharts";
 
 const HourlyForecast = ({ hourlyForecast }) => {
+    const [unit, setUnit] = useState("°C");
+
+    //convert temperatures
+    const convertTemp = (tempC) => {
+        return unit === "°F" ? (tempC * 9) / 5 + 32 : tempC;
+    }
+
+    //apply conversion to the data
+    const convertedData = hourlyForecast.map(item => ({
+        ...item, 
+        temp: convertTemp(item.temp)
+    }));
+
     return (
         <div className="chart-container">
+
+            <button onClick={() => setUnit(unit === "°C" ? "°F" : "°C")} className="unit-toggle-button">
+                Show {unit === "°C" ? "°F" : "°C"}
+            </button>
+
             <ResponsiveContainer width="100%" height={180}>
-                <AreaChart data={hourlyForecast} cursor="pointer">
+                <AreaChart data={convertedData} cursor="pointer">
                     <XAxis dataKey="time" />
-                    <YAxis domain={["auto", "auto"]} unit="°C"/>
-                    <Tooltip cursor={false} />
-                    <Area type="natural" dataKey="temp" unit="°C" stroke="#FFBF00" strokeWidth={3} fill="#FFBF00" fillOpacity={0.3} />
+                    <YAxis domain={["auto", "auto"]} unit={`${unit}`}/>
+                    <Tooltip cursor={false} formatter={(value) => [`${value.toFixed(1)}`, 'Temp']} />
+                    <Area type="linear" dataKey="temp" unit={`${unit}`} stroke="#FFBF00" strokeWidth={3} fill="#FFBF00" fillOpacity={0.3} />
                 </AreaChart>
             </ResponsiveContainer>
         </div>
